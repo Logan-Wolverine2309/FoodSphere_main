b0b69922a60cbc06b53bdea34dcefc72aa1e52bc
No Time <jai9674yaaa@gmail.com>
2025-06-14 21:16:13 +0530

    updated gateway

--- src/AdminComponent/util/Api.js
+++
@@ -1,9 +0,0 @@
-// utils/api.js
-export const getRestaurantByOwner = async () => {
-  const restaurant = localStorage.getItem('restaurant');
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({ restaurant: restaurant ? JSON.parse(restaurant) : null });
-    }, 500);
-  });
-};
--- src/component/config/logic.js
+++
@@ -1,9 +0,0 @@
-
-export const isPresentInFavourites = (favourites, restaurant) => {
-    for (let item of favourites) {
-        if (restaurant.id === item.id) {
-            return true
-        }
-    }
-    return false;   
-}
